66659752bb6edaae31eecc03bcf75e8562e8b855
james <jamboyoyo@gmail.com>
2025-09-10 11:49:29 -0400

    Added watchlist, when movies toggled, they show on watchlist page.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../styles.css";
 
-export default function MovieCard({ movie, isWatchListed, toggleWatchList }) {
+export default function MovieCard({ movie, isWatchlisted, toggleWatchlist }) {
   const handleError = (e) => {
     e.target.src = "images/default.jpg";
   };
 
   const getRatingClass = (rating) => {
     if (rating >= 8) return "rating-good";
-    if (rating >= 5) return "rating-ok";
+    if (rating >= 5 && rating < 8) return "rating-ok";
     return "rating-bad";
   };
 
@@ -21,10 +21,24 @@ export default function MovieCard({ movie, isWatchListed, toggleWatchList }) {
       />
       <div className="movie-card-info">
         <h3 className="movie-card-title">{movie.title}</h3>
-        <p className="movie-card-genre">{movie.genre}</p>
-        <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
-          {movie.rating}
-        </p>
+        <div>
+          <span className="movie-card-genre">{movie.genre}</span>
+          <span className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+            {movie.rating}
+          </span>
+        </div>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={isWatchlisted}
+            onChange={() => toggleWatchlist(movie.id)}
+          ></input>
+          <span className="slider">
+            <span className="slider-label">
+              {isWatchlisted ? "In Watchlist" : "Add to Watchlist"}
+            </span>
+          </span>
+        </label>
       </div>
     </div>
   );
