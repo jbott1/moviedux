d3e86a5539a63043c5f33010d8aea78a29013021
james <jamboyoyo@gmail.com>
2025-09-10 11:10:17 -0400

    Added router and watchlist page

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles.css";
 
-export default function MovieCard({ movie }) {
+export default function MovieCard({ movie, isWatchListed, toggleWatchList }) {
   const handleError = (e) => {
     e.target.src = "images/default.jpg";
   };
--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,18 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "../styles.css";
 import MovieCard from "./MovieCard";
 
-export default function MoviesGrid() {
-  const [movies, setMovies] = useState([]);
+export default function MoviesGrid({ movies, watchlist, toggleWatchlist }) {
   const [searchTerm, setSearchTerm] = useState("");
-  const [genre, setGenre] = useState(["All genres"]);
-  const [rating, setRating] = useState(["All"]);
 
-  useEffect(() => {
-    fetch("movies.json")
-      .then((response) => response.json())
-      .then((data) => setMovies(data));
-  }, []);
+  const [genre, setGenre] = useState("All Genres");
+  const [rating, setRating] = useState("All");
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -28,7 +22,7 @@ export default function MoviesGrid() {
 
   const matchesGenre = (movie, genre) => {
     return (
-      genre === "All genres" ||
+      genre === "All Genres" ||
       movie.genre.toLowerCase() === genre.toLowerCase()
     );
   };
@@ -64,7 +58,7 @@ export default function MoviesGrid() {
       <input
         type="text"
         className="search-input"
-        placeholder="Search Movies..."
+        placeholder="Search movies..."
         value={searchTerm}
         onChange={handleSearchChange}
       />
@@ -77,7 +71,7 @@ export default function MoviesGrid() {
             value={genre}
             onChange={handleGenreChange}
           >
-            <option>All genres</option>
+            <option>All Genres</option>
             <option>Action</option>
             <option>Drama</option>
             <option>Fantasy</option>
@@ -101,7 +95,12 @@ export default function MoviesGrid() {
 
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
+          <MovieCard
+            movie={movie}
+            key={movie.id}
+            toggleWatchlist={toggleWatchlist}
+            isWatchlisted={watchlist.includes(movie.id)}
+          />
         ))}
       </div>
     </div>
